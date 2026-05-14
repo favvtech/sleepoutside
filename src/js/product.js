84b6a403ae9f8d538b2efe3add2c68b39b2c9d03
johannvasquez05 <jvquezmadero@byupathway.edu>
2026-05-13 22:33:50 -0500

    team modifactions week02

--- src/js/product.js
+++ src/js/product.js
@@ -1,7 +1,9 @@
 import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import { getParam } from "./utils.mjs";
 
 const dataSource = new ProductData("tents");
+const productId = getParam('product');
 
 function addProductToCart(product) {
   const storedCart = getLocalStorage("so-cart");
@@ -23,3 +25,5 @@ async function addToCartHandler(e) {
 document
   .getElementById("addToCart")
   .addEventListener("click", addToCartHandler);
+
+console.log(dataSource.findProductById(productId));
